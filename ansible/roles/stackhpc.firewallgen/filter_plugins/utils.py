7aa746e41771d4fc26f51a4a8674be9b3baf33e4
Will Szumski <will@stackhpc.com>
2018-08-17 11:52:43 +0100

    add add custom rules to the generated output

--- ansible/roles/stackhpc.firewallgen/filter_plugins/utils.py
+++ ansible/roles/stackhpc.firewallgen/filter_plugins/utils.py
@@ -19,6 +19,28 @@ def keyvalue_dict(data):
 def single_quote(string):
     return "'" + string + "'"
 
+def socket_to_rule(socket):
+    docker_hint = ''
+    if socket['processes'][0]['docker_hint']:
+        docker_hint = " in docker container '{}'".format(
+            socket['processes'][0]['docker_hint']
+        )
+    comment = "hint: used by '{process}'{docker_hint}".format(
+        process=socket['processes'][0]['name'],
+        docker_hint=docker_hint
+        )
+    return {
+        'interface': socket["interface"],
+        'port': socket["port"],
+        'destination': socket["ip"],
+        'comment': comment,
+        'proto': socket["proto"],
+    }
+
+
+def firewallgen_rules(sockets):
+    return [ socket_to_rule(socket) for socket in sockets]
+
 class FilterModule(object):
     ''' Query filter '''
 
@@ -26,5 +48,6 @@ class FilterModule(object):
         return {
             'sort_multi': sort_multi,
             'keyvalue_dict': keyvalue_dict,
-            'single_quote': single_quote
+            'single_quote': single_quote,
+            'firewallgen_rules': firewallgen_rules
         }
